4008575193604718b4985a556442abede6c9b2cb
Junaid Ahmed <junaidahmed9038@gmail.com>
2023-05-12 08:31:01 +0530

    create action for reducer

---
+++ src/components/App.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { data } from "../data"; // remove data - get data from store
+import Navbar from "./Navbar";
+import MovieCard from "./MovieCard";
+
+class App extends React.Component {
+
+  componentDidMount () {
+    const {store} = this.props;
+    store.subscribe(() =>{
+      console.log('UPDATED');
+      this.forceUpdate();
+    })
+    //make call 
+    //dispatch action
+    store.dispatch({
+      type :'ADD_MOVIES',
+      movies:data
+    });
+
+    console.log('STATE',this.props.store.getState());
+  }
+
+ 
+
+  render (){
+    
+    const movies = this.props.store.getState();
+    console.log('RENDER')
+  return (
+    <div className="App">
+      <Navbar/>
+      <div className="main">
+        <div className="tabs">
+          <div className="tab"> Movie</div>
+          <div className="tab"> Favourites </div>
+        </div>
+
+        <div className="list">
+           {movies.map((movie,index) =>(
+            <MovieCard movie ={movie} key = {`movies-${index}`} />
+           ))}
+        </div>
+      </div>
+    </div>
+  );
+
+  }
+  
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createStore } from 'redux';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './components/App';
+import movies from './reducers';
+
+
+
+const store = createStore(movies);
+// const store = configureStore(movies);
+console.log('store',store);
+// console.log('BEFOR STATE',store.getState());
+
+// store.dispatch({
+//   type:'ADD_MOVIES',
+//   movies:[{name:'supperman'}]
+
+// });
+
+// console.log('AFTER STATE',store.getState());
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App store = {store} />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
